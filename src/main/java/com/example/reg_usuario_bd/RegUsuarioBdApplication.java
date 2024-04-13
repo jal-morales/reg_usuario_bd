8c1e68c8e0242bc458f93cec50acace4d5f579f6
jal-morales <jal.morales89@gmail.com>
2024-04-12 22:51:13 -0400

    first commit

---
+++ src/main/java/com/example/reg_usuario_bd/RegUsuarioBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.reg_usuario_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegUsuarioBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegUsuarioBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reg_usuario_bd/RegUsuarioBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reg_usuario_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegUsuarioBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
