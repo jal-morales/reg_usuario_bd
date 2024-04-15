edcbfdd79ad08eef4bc46a24168d6f0a06f9ac6e
jal-morales <jal.morales89@gmail.com>
2024-04-15 00:50:57 -0400

    metodo eliminar agregado

--- src/main/java/com/example/reg_usuario_bd/controller/RegistroUsuarioController.java
+++ src/main/java/com/example/reg_usuario_bd/controller/RegistroUsuarioController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,16 @@ ResponseLogin responseLogin;
 public List<RegistroUsuario> getAllRegistroUsuarios() {
     return registroUsuarioServices.getAllRegistroUsuarios();
 } 
+
+@DeleteMapping("/{id}")
+public void deleteUsuarios(@PathVariable Integer id){
+    try {
+        registroUsuarioServices.deleteUsuarios(id);
+        new ResponseEntity<>("Usuario  Eliminado Correctamente",HttpStatus.OK);     
+    } catch (Exception e) {
+        new ResponseEntity<>("Error al eliminar la pelicula id:"+id,HttpStatus.BAD_REQUEST);  
+    }     
+}
 
 @PostMapping("/login")
 public ResponseEntity<ResponseLogin> login (@RequestBody RequestLogin usuario) {
