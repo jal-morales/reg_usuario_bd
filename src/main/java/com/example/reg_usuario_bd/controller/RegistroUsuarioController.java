a54c1dd0fccbbacdcfab475e6a7098de505eccc4
jal-morales <jal.morales89@gmail.com>
2024-04-25 12:57:15 -0400

    Se implementa hateoas

--- src/main/java/com/example/reg_usuario_bd/controller/RegistroUsuarioController.java
+++ src/main/java/com/example/reg_usuario_bd/controller/RegistroUsuarioController.java
@@ -1,6 +1,8 @@
 package com.example.reg_usuario_bd.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,24 +21,46 @@ import com.example.reg_usuario_bd.model.RequestLogin;
 import com.example.reg_usuario_bd.model.ResponseLogin;
 import com.example.reg_usuario_bd.service.RegistroUsuariosServices;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 @RestController
 @RequestMapping("/usuarios")
 public class RegistroUsuarioController {
+    private static final Logger log = LoggerFactory.getLogger(RegistroUsuarioController.class);
 @Autowired
 private RegistroUsuariosServices registroUsuarioServices;
 
 @Autowired
 ResponseLogin responseLogin;
 
+/*
 @GetMapping
 public List<RegistroUsuario> getAllRegistroUsuarios() {
     return registroUsuarioServices.getAllRegistroUsuarios();
-} 
+} */
+@GetMapping
+public CollectionModel<EntityModel<RegistroUsuario>> getAllRegistroUsuarios() {
+    List<RegistroUsuario> registroUsuarios = registroUsuarioServices.getAllRegistroUsuarios();
+    log.info("GET /usuarios");
+    log.info("Retornando todos los usuarios");
+
+    List<EntityModel<RegistroUsuario>> registroUsuarioResourse = registroUsuarios.stream()
+            .map(registroUsuario -> EntityModel.of(registroUsuario, WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass())
+                    .getRegistroUsuarioById(registroUsuario.getId_ru())).withSelfRel()))
+            .collect(Collectors.toList());
+
+    WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllRegistroUsuarios());
+    CollectionModel<EntityModel<RegistroUsuario>> resources = CollectionModel.of(registroUsuarioResourse, linkTo.withRel("registroUsuario"));
+
+    return resources;
+}
 
 /*CREAR  */
 @PostMapping
@@ -50,6 +74,21 @@ public RegistroUsuario modificarPelicula(@PathVariable int id, @RequestBody Regi
     
     
     return registroUsuarioServices.updateUsuario(id, registroUsuario);
+}
+
+/*Get UsuarioBYID */
+@GetMapping("/{id_registroUsuario}")
+public EntityModel<RegistroUsuario>  getRegistroUsuarioById(@PathVariable int id_registroUsuario) {
+
+
+    Optional<RegistroUsuario> registroUsuario = registroUsuarioServices.getRegistroUsuarioById(id_registroUsuario);
+    if (registroUsuario.isPresent()) {
+        return EntityModel.of(registroUsuario.get(),
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getRegistroUsuarioById(id_registroUsuario)).withSelfRel(),
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllRegistroUsuarios()).withRel("all-RegistroUsuario"));
+    } else {
+        throw new RegistroUsuarioNotFoundException("id usuario no existe: " + id_registroUsuario);
+    } 
 }
 
 
